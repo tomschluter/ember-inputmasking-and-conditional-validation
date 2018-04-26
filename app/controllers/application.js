0e15e233fa6fe435a7e04cc143ada8f00cc2d6a1
Tom Schluter <TomSchluter@ccveu.local>
2018-04-26 11:43:11 +0200

    commented some code and added some more bootstrap

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -7,13 +7,22 @@ export default Controller.extend({
     init(){
         this._super(...arguments);
         let model = {};
+
+        // define a changeset that we'll use in the form to handle validations before allowing to submit.
         let changeset = new Changeset(model, lookupValidator(Validations), Validations);
         this.set('changeset', changeset);
+
+        // Set options for ember-inputmask
+        let options = {
+            placeholder:"-"
+        }
+        this.set("options", options);
     },
 
     actions: {
         submit(model) {
             alert('submitted');
+            // Show that the submit was succesful by logging the value of inputMask.
             console.log(model.get("inputMask"));
         }
       }
--- app/validations/settings.js
+++ app/validations/settings.js
@@ -5,6 +5,7 @@ import {
   } from 'ember-changeset-validations/validators';
 import validateSometimes from 'ember-changeset-conditional-validations/validators/sometimes';
   
+  
   export default {
     firstName: [
       validatePresence(true),
@@ -14,10 +15,14 @@ import validateSometimes from 'ember-changeset-conditional-validations/validator
       validatePresence(true),
       validateLength({ min: 2 })
     ],
+
+    // Validate the value of inputMask as you would validate any other input.
     inputMask: [
         validatePresence(true),
         validateLength({ is: 6})
     ],
+
+    // Use validateSometimes to only validate description when enterDescription is true (the checkbox is checked).
     description: validateSometimes([
         validatePresence(true),
         validateLength({ is: 4 })
